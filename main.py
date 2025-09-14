c4a01357fccb6ddfedea60d09dbb8513254bfc93
jessy836 <neto@pc-ernesto.>
2025-09-14 09:38:45 +0200

    feat(data): Add CSV files for 2020 municipal
    Create and load data for each arrondissements from the 2020 French municipal elections into a dictionary for easier access. The data files include results for each polling station and are stored in a structured format.
    
    Add initial setup in main.py for processing these datasets.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("data/municipales_2020.csv")
+
+# essayons de  charger ldes données pour les municipales 2020
+municipales_2020 = dict()
+
+for i in range(1,20):
+    numero_arrandissement=f"{i:02}"
+    nom_df=f"arrandissement_{numero_arrandissement}"
+    nom_fichier=f"./data/municipales_2020/ddct_berp_municipales_2020_tour1_ardt_{numero_arrandissement}_20200315.csv"
+    municipales_2020[nom_df] = pd.read_csv(
+        nom_fichier,
+    )
